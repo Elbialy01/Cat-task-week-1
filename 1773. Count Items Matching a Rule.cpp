0d027ba5fb7bba7e02c94b4fc1902a23663f7b39
Mahmoud Elbialy <119286962+Elbialy01@users.noreply.github.com>
2022-12-14 09:03:33 +0200

    Create 1773. Count Items Matching a Rule.cpp

---
+++ 1773. Count Items Matching a Rule.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int countMatches(vector<vector<string>>& items, string ruleKey, string ruleValue) {
+        int n=0;
+        int j;
+        if(ruleKey=="type")
+        j=0;
+        else if(ruleKey=="color")
+        j=1;
+        else j=2;
+        for(int i=0;i<items.size();i++){
+                if(items[i][j]==ruleValue)
+                n++;
+            
+        }
+        return n;
+    }
+};
